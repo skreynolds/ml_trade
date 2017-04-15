89d5220992532c5ec69b6f31cf3c6c7df6a531ce
skreynolds <shane.k.reynolds@gmail.com>
2017-04-15 23:56:04 +0930

    Add a script which plots a different set of data, selected from a dataframe using index selection

---
+++ matplotlib_scripts/plotting_data_high.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 21:19:27 2017
+
+@author: shane
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def test_run():
+    df = pd.read_csv("~/Documents/ml_trade/data/IBM.csv")
+    df["High"].plot()
+    plt.show()
+    
+if __name__ == "__main__":
+    test_run()
