9be963c2453e664df0feed7b830dedf0de5c471b
skreynolds <shane.k.reynolds@gmail.com>
2017-04-15 23:57:11 +0930

    Add a script which plots two data series from pandas dataframe

---
+++ matplotlib_scripts/plot_two_series.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 21:24:12 2017
+
+@author: shane
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def test_run():
+    df = pd.read_csv("~/Documents/ml_trade/data/AAPL.csv")
+    df[['Close','High']].plot()
+    plt.show()
+
+if __name__ == "__main__":
+    test_run()
