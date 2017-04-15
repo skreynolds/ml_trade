d620916504a38bad06708c9f54adb4b8efcfa599
skreynolds <shane.k.reynolds@gmail.com>
2017-04-15 17:32:44 +0930

    Add a script which find the max closing value of a csv data file loaded into a pandas data frame

---
+++ pandas_scripts/get_max_close.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 17:15:58 2017
+
+@author: shane
+"""
+
+import pandas as pd
+
+def get_max_close(symbol):
+    """
+    Return the maximum closing value for stock indicated
+    by symbol. Note: data for a stock is stored in the
+    file: data/<symbol>.csv
+    """
+    df = pd.read_csv("data/{}.csv".format(symbol)) #read in the data
+    return df['Close'].max() #computer and return max
+
+def test_run():
+    """Function called by Test Run"""
+    for symbol in ['AAPL','IBM']:
+        print("Max close")
+        print(symbol, get_max_close(symbol))
+        
+if __name__ == "__main__":
+    test_run()
