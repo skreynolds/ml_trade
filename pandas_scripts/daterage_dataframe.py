dd28dc4f57436bbca2d03f4682124fdbbab40f5c
skreynolds <shane.k.reynolds@gmail.com>
2017-04-15 23:50:23 +0930

    Add a script which creates a daterage object and a temporary dataframe populated with the dates in the daterange

---
+++ pandas_scripts/daterage_dataframe.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 21:33:37 2017
+
+@author: shane
+"""
+
+import pandas as pd
+
+def test_run():
+    start_date = '2016-04-18'
+    end_date = '2017-04-13'
+    dates = pd.date_range(start_date,end_date)
+    
+    #Create an empty dataframe
+    df1 = pd.DataFrame(index=dates)
+    
+    #Read SPY data into temporary dataframe
+    dfSPY = pd.read_csv("~/Documents/ml_trade/data/SPY.csv",
+                        index_col="Date",
+                        parse_dates=True,
+                        usecols=['Date','Adj Close'],
+                        na_values=['nan'])
+
+    #Join the two dataframes using DataFrame.join()
+    df1 = df1.join(dfSPY)
+    
+    #Drop NaN values
+    df1 = df1.dropna()
+    
+if __name__ == "__main__":
+    test_run()
